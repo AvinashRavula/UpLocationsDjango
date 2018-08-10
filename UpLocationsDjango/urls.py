e4bb2d5ae4cd467f50f9a02ccccc1d1cb9b443b8
AvinashRavula <avinashravula@yahoo.com>
2018-08-10 17:37:35 +0530

    Added api to the url

--- UpLocationsDjango/urls.py
+++ UpLocationsDjango/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('uplocations/api/', include('RestAPIs.urls')),
+    path('api/', include('RestAPIs.urls')),
 ]
